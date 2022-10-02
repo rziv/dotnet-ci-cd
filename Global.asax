e4e3e259b0637ffc7c26ed475bc06fabb2c8f6d5
Ran-Ziv <Ran.Ziv@mail.huji.ac.il>
2022-10-02 10:11:58 +0300

    changing project structure

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="calendar_matrix.Global" Language="C#" %>
---
+++ RoomModel.cs
@@ -0,0 +1,8 @@
+﻿namespace calendar_matrix
+{
+    public class RoomModel
+    {
+        public string BuildingName { get; set; }
+        public string Name { get; set; }
+    }
+}
